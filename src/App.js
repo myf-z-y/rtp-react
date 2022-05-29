0e01d9562766ceab8859c8b71317022690f1a419
myf-z-y <1684963183@qq.com>
2022-05-29 21:01:45 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import figImg from './figshare.png';
+import Footer from './components/Footer';
+import long_res from './pdfs/read_screen_result_long.pdf';
+import middle_res from './pdfs/read_screen_result_middle.pdf'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="logo">
+          Machine-learning-assisted high-throughput screening for organic
+          room-temperature phosphorescent molecules
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Blog</a>
+            </li>
+            <li>
+              <a href="">About us</a>
+            </li>
+            <li>
+              <a href="">Our history</a>
+            </li>
+            <li>
+              <a href="">Contacts</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main className="grid">
+        <article>
+          <h1>Introduction</h1>
+          <p>
+            This project is created for high-throughput screening of promising
+            room temperature phosphorescence (RTP). The training set is collected
+            from the literature, which is expanded to serve as a better sample of
+            chemical space. The dataset for screening is from PubChem, which is
+            converted to 45 million valid SMILES and uploaded to
+            <a href="https://doi.org/10.6084/m9.figshare.19397522.v1" title="data from figshare"><img src={figImg}
+              alt="figshare" className="fig" /></a>
+            (as the data is large).
+          </p>
+          <div className="notecard">
+            <p>
+              <strong>Note</strong>: users need to download and unzip the
+              compressed file to the folder <strong>smiles_bit_pickle</strong> if
+              screening is required.
+            </p>
+          </div>
+
+          <h3>Table of Contents</h3>
+          <ul>
+            <li><a href="#req">Requirements</a></li>
+            <li><a href="#pdf">PDFs</a></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <h2 id="req">Requirements</h2>
+          <h2 id="pdf">PDFs</h2>
+          <p>
+            <a href={long_res}>See long class result (PDF, 5.93 MB)</a>
+          </p>
+          <p>
+            <a href={middle_res}>See middle class result (PDF, 6.11 MB)</a>
+          </p>
+          <h2>E-mail</h2>
+          <a href="mailto:iammyf@njtech.edu.cn">Send email to myf</a>
+        </article>
+        <aside className="toc">
+          <h2>hi</h2>
+        </aside>
+
+
+      </main>
+      <Footer />
+
     </div>
   );
 }
