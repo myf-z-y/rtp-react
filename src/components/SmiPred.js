7984091423e845dfae403852b416187adeb1eb94
zjmyf <1684963183@qq.com>
2022-06-01 16:43:36 +0800

    add smiVig

--- src/components/SmiPred.js
+++ src/components/SmiPred.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 
 export default function SmiPred(props) {
   const [newName, setNewName] = useState("");
   const [res, setRes] = useState("");
   const [prob, setProb] = useState("");
   const [stat, setStat] = useState("");
+  const [inputSmi, setInputSmi] = useState("");
+  const [imgSrc, setImgSrc] = useState("");
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -14,30 +17,50 @@ export default function SmiPred(props) {
     e.preventDefault();
 
     try {
+      const genNano = nanoid();
       // after this line, our function will wait for the `fetch()` call to be settled
       // the `fetch()` call will either return a Response or throw an error
       //const response = await fetch('/predict?smi=${newName}`);
       setStat("computing...");
-      const response = await fetch("/predict", {
+      const responsePred = await fetch("/predict", {
         method: "post",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ smi: newName }),
       });
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
+
+      const responseImg = await fetch("/smiVis", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ smi: newName, randId: genNano }),
+      });
+
+      const responseClean = await fetch("/cleanImg", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ smi: newName, randId: genNano }),
+      });
+      /*
+      if (!(responsePred.ok && responseImg.ok && responseClean.ok)) {
+        throw new Error(`HTTP error: ${responsePred.status}`);
       }
+      */
       // after this line, our function will wait for the `response.json()` call to be settled
       // the `response.json()` call will either return the JSON object or throw an error
-      const json = await response.json();
-      
+      const json = await responsePred.json();
+
       setRes(json["res"]);
       if (json["prob"] === -1) {
         setProb("");
       } else {
         setProb(json["prob"]);
       }
-      setStat("done!")
+      setStat("done!");
+      setInputSmi(newName);
       //console.log(json);
+
+      const blob = await responseImg.blob();
+      const objectURL = URL.createObjectURL(blob);
+      setImgSrc(objectURL);
     } catch (error) {
       console.error(`Could not get products: ${error}`);
     }
@@ -59,8 +82,16 @@ export default function SmiPred(props) {
         <span className="status">Status: {stat}</span>
       </div>
       <div className="pred-result">
-        <p>result: {res}</p>
-        <p>prob: {prob}</p>
+        <p>input: </p>
+        <div className="inputSmi">{inputSmi}</div>
+        <p>result: </p>
+        <div className="result">{res}</div>
+        <p>prob: </p>
+        <div className="prob">{prob}</div>
+        <p>vis:</p>
+        <div className="smiVis">
+          <img src={imgSrc} alt=""></img>
+        </div>
       </div>
     </form>
   );
